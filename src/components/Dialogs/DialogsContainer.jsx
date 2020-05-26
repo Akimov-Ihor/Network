548112f4256d9bbd810265191ef1ce5fa3411cd0
Akimov-Ihor <pandable1996@gmail.com>
2020-05-26 18:31:30 +0300

    [Add]Compose redux and changeable status

--- src/components/Dialogs/DialogsContainer.jsx
+++ src/components/Dialogs/DialogsContainer.jsx
@@ -2,15 +2,16 @@ import React from "react"
 import Dialogs from './Dialogs'
 import { sendMesssageCreator, updateNewMessageBodyCreator } from "../../redux/dialogs-reducer"
 import { connect } from "react-redux";
+import { withAuthRedirect } from "../../hoc/withAuthRedirect";
+import { compose } from "redux";
 
 
 
 let mapStateToProps =(state)=>{
-    console.log(state);
+   
     return{
         dialogsPage:state,
-        isAuth:state.auth.isAuth
-
+       
     }
 }
 let mapDispatchToProps = (dispatch)=>{
@@ -23,7 +24,13 @@ let mapDispatchToProps = (dispatch)=>{
     }
   }
 }
-const DialogsContainer= connect(mapStateToProps,mapDispatchToProps) (Dialogs)
 
 
-export default DialogsContainer;
+
+
+
+
+export default compose(
+  connect(mapStateToProps,mapDispatchToProps) ,
+  withAuthRedirect
+)(Dialogs);
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -3,6 +3,8 @@ import Users from './Users';
 import { connect } from 'react-redux';
 import { follow, unfollow, setCurrentPage,toggleFollowingProgress, getUsers } from '../../redux/users-reducer';
 import Preloader from '../Preloader/Preloader';
+import { withAuthRedirect } from '../../hoc/withAuthRedirect';
+import { compose } from 'redux';
 
 
 
@@ -47,5 +49,7 @@ let mapStateToProps = (state) => {
 }
 
 
-export default connect(mapStateToProps, 
-    { follow, unfollow,  setCurrentPage, toggleFollowingProgress,getUsers })(UsersContainer);
+export default compose (
+    withAuthRedirect,
+    connect(mapStateToProps, { follow, unfollow,  setCurrentPage, toggleFollowingProgress,getUsers })
+)(UsersContainer)
